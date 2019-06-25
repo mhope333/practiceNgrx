d6e48b0470f1eec84c0861fe24c718b53c4e78c1
Hope <hopem@bglm33091.b2b.regn.net>
2019-06-25 15:37:03 +0100

    styling

--- src/app/models/AppState.ts
+++ src/app/models/AppState.ts
@@ -3,7 +3,6 @@ import { User } from './user';
 // this is the Applications Store State: (store is the appState Object{})
 export interface AppState {
   // user slice of state object in the store will contain the users Array of objects
-  // usersStateSlice: { users: User[] };
   readonly users: { users: User[] }; // ???? WHY
   // readonly users: User[]; // ???
 }
